d8be97a026e401feefdeacfd4cdf3a6b9f745801
brunoscholz <bruno@oction.com.br>
2017-01-19 09:07:10 -0200

    tour done

--- src/pages/tour/tour.ts
+++ src/pages/tour/tour.ts
@@ -17,27 +17,27 @@ export class TourPage {
     this.logged = navParams.get('logged');
     this.slides = [
       {
-        title: "Bem Vindo ao OndeTem?!",
-        description: "Your dream house is one tap away! Open the menu and select <strong>Properties</strong> to start your search.",
-        image: "assets/img/splash/house.svg",
+        title: "Bem Vindo ao Onde tem?!",
+        description: "Ficamos alegres em poder ajudar você a realizar buscar mais eficientes no seu dia a dia e assim otimizar seu tempo.",
+        image: "assets/img/splash/time.svg",
         class: 'first-intro-slide',
       },
       {
-        title: "O que é o OndeTem?",
-        description: "Ionic Realty has the best brokers in the business. Open the menu and select <strong>Brokers</strong> to connect with our brokers in a whole new way!",
-        image: "assets/img/splash/brokers.svg",
+        title: "O que é o Onde tem?",
+        description: "É uma plataforma que aproxima clientes e empresas, facilitando a comunicação e os negócios...",
+        image: "assets/img/splash/chat.svg",
         class: 'second-intro-slide',
       },
       {
         title: "Como funciona?",
-        description: "Keep track of your <strong>favorites</strong> and get notified when events happen: price reductions, open houses, etc.",
-        image: "assets/img/splash/favorites.svg",
+        description: "O cliente pode receber notificações sobre ofertas de produtos e serviços e encontrar as melhores ofertas sem sair de casa!",
+        image: "assets/img/splash/favorite.svg",
         class: 'third-intro-slide',
       },
       {
         title: "Moedas",
-        description: "Keep track of your <strong>favorites</strong> and get notified when events happen: price reductions, open houses, etc.",
-        image: "assets/img/splash/favorites.svg",
+        description: "Ao utilizar o app, você ganha moedas que podem ser trocadas por algumas ofertas das empresas participantes!",
+        image: "assets/img/splash/coins.svg",
         class: 'forth-intro-slide',
       }
     ];
